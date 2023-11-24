82b9e665655c2d12d3ee474e7c6510d4a3198ded
amansaraswat177 <amansarswat786@gmail.com>
2023-11-25 01:31:00 +0530

    Implementation Of History Fragment Of User Panel Completed

--- app/src/main/java/com/example/munchmate/Fragment/HistoryFragment.kt
+++ app/src/main/java/com/example/munchmate/Fragment/HistoryFragment.kt
@@ -5,24 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.munchmate.R
-
+import com.example.munchmate.adapter.BuyAgainAdapter
+import com.example.munchmate.databinding.FragmentHistoryBinding
 
 class HistoryFragment : Fragment() {
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var binding: FragmentHistoryBinding
+    private lateinit var buyAgainAdapter: BuyAgainAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentHistoryBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_history, container, false)
+        setupRecyclerView()
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+    }
 
-
+    private fun setupRecyclerView() {
+        val buyAgainFoodName = arrayListOf("Food 1", "Food 2", "Food 3")
+        val buyAgainFoodPrice = arrayListOf("$10", "$32", "$12")
+        val buyAgainFoodImage = arrayListOf(R.drawable.menu1, R.drawable.menu2, R.drawable.menu3)
+        buyAgainAdapter = BuyAgainAdapter(buyAgainFoodName, buyAgainFoodPrice, buyAgainFoodImage)
+        binding.buyAgainRecyclerView.adapter = buyAgainAdapter
+        binding.buyAgainRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+    }
 }
